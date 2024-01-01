50dd49b5017a9bf33f9d4e034bb4799ed2079c28
hiven <mail@hiven.com>
2024-01-01 16:02:37 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,54 +1,31 @@
 import os
-from dotenv import load_dotenv
 from flask import Flask, render_template, abort, redirect, request
 import stripe
-
-load_dotenv()
 
 app = Flask(__name__)
 stripe.api_key = os.environ['STRIPE_SECRET_KEY']
 
-products = {
-    'megatutorial': {
-        'name': 'The Flask Mega-Tutorial',
-        'price': 3900,
-    },
-    'support': {
-        'name': 'Python 1:1 support',
-        'price': 20000,
-        'per': 'hour',
-        'adjustable_quantity': {
-            'enabled': True,
-            'minimum': 1,
-            'maximum': 4,
-        },
-    },
-}
-
+product_name = 'The Flask Mega-Tutorial'
+product_price = 3900  # Price in cents
 
 @app.route('/')
 def index():
-    return render_template('index.html', products=products)
+    return render_template('index.html', product_name=product_name, product_price=product_price)
 
 
-@app.route('/order/<product_id>', methods=['POST'])
-def order(product_id):
-    if product_id not in products:
-        abort(404)
-
+@app.route('/order', methods=['POST'])
+def order():
     checkout_session = stripe.checkout.Session.create(
         line_items=[
             {
                 'price_data': {
                     'product_data': {
-                        'name': products[product_id]['name'],
+                        'name': product_name,
                     },
-                    'unit_amount': products[product_id]['price'],
+                    'unit_amount': product_price,
                     'currency': 'usd',
                 },
                 'quantity': 1,
-                'adjustable_quantity': products[product_id].get(
-                    'adjustable_quantity', {'enabled': False}),
             },
         ],
         payment_method_types=['card'],
@@ -83,11 +60,11 @@ def new_event():
         abort(400)
 
     if event['type'] == 'checkout.session.completed':
-      session = stripe.checkout.Session.retrieve(
-          event['data']['object'].id, expand=['line_items'])
-      print(f'Sale to {session.customer_details.email}:')
-      for item in session.line_items.data:
-          print(f'  - {item.quantity} {item.description} '
-                f'${item.amount_total/100:.02f} {item.currency.upper()}')
+        session = stripe.checkout.Session.retrieve(
+            event['data']['object'].id, expand=['line_items'])
+        print(f'Sale to {session.customer_details.email}:')
+        for item in session.line_items.data:
+            print(f'  - {item.quantity} {item.description} '
+                  f'${item.amount_total/100:.02f} {item.currency.upper()}')
 
     return {'success': True}
